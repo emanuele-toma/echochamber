df68a6c36a71a5c84e1bae4548e7c5058828abbc
emanuele-toma <toma.emanuele03@gmail.com>
2024-11-27 14:09:21 +0100

    :card_file_box: Fix db user creation

--- mongo-init.js
+++ mongo-init.js
@@ -1,9 +1,11 @@
+db = db.getSiblingDB(process.env.MONGO_DB);
+
 db.createUser({
   user: process.env.MONGO_USER,
   pwd: process.env.MONGO_PASS,
   roles: [
     {
-      role: "readWrite",
+      role: 'readWrite',
       db: process.env.MONGO_DB,
     },
   ],
